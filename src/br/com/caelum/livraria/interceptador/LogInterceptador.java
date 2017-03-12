dcaecf4a77a4822cc2d6b22af10b0f7cfdb75125
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-12 16:33:38 -0300

    C2M6.09 - Tempo de acesso ao banco

---
+++ src/br/com/caelum/livraria/interceptador/LogInterceptador.java
@@ -0,0 +1,18 @@
+package br.com.caelum.livraria.interceptador;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+public class LogInterceptador {
+
+	@AroundInvoke
+	public Object intercepta(InvocationContext contex) throws Exception {
+		
+		long millis = System.currentTimeMillis();
+		Object o = contex.proceed();
+
+		System.out.println("[INFO] Tempo gasto no acesso ao BD: " + (System.currentTimeMillis() - millis) + "ms");
+
+		return o;
+	}
+}

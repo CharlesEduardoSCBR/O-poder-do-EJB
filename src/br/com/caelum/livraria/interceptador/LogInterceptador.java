224c4c86b52d43f6858aec5ea3079575f0712cee
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-12 16:47:03 -0300

    C2M6.10 - Melhorando a saída

--- src/br/com/caelum/livraria/interceptador/LogInterceptador.java
+++ src/br/com/caelum/livraria/interceptador/LogInterceptador.java
@@ -11,6 +11,10 @@ public class LogInterceptador {
 		long millis = System.currentTimeMillis();
 		Object o = contex.proceed();
 
+		String nomeClasse = contex.getTarget().getClass().getSimpleName();
+		String nomeMetodo = contex.getMethod().getName();
+		
+		System.out.println("[INFO] " + nomeClasse + "->" + nomeMetodo);
 		System.out.println("[INFO] Tempo gasto no acesso ao BD: " + (System.currentTimeMillis() - millis) + "ms");
 
 		return o;
